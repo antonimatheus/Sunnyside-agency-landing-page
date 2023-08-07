4d71fadf6e93800f467c1084609f2af74eb98b55
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-08-07 11:31:42 -0400

    ClientTestimonials done

--- src/App.jsx
+++ src/App.jsx
@@ -50,27 +50,30 @@ function App() {
           />
         </div>
 
-        <ClientTestimonials 
-        img='./src/assets/images/image-emily.jpg'
-        description='We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
-        name='Emily R.'
-        job='Marketing Director'
-        />
-
-        <ClientTestimonials 
-        img='./src/assets/images/image-thomas.jpg'
-        description='Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
-        name='Thomas S.'
-        job='Chief Operating Officer'
-        />
-
-        <ClientTestimonials 
-        img='./src/assets/images/image-jennie.jpg'
-        description='Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
-        Jennie F.'
-        name='Jhennie F.'
-        job='Businees Owner'
-        />
+        <div className='ClientTestimonials--App'>
+          <h1>Client testimonials</h1>
+          <div className='ClientTestimonials--AppBlock'>
+            <ClientTestimonials
+            img='./src/assets/images/image-emily.jpg'
+            description='We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
+            name='Emily R.'
+            job='Marketing Director'
+            />
+            <ClientTestimonials
+            img='./src/assets/images/image-thomas.jpg'
+            description='Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
+            name='Thomas S.'
+            job='Chief Operating Officer'
+            />
+            <ClientTestimonials
+            img='./src/assets/images/image-jennie.jpg'
+            description='Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+            Jennie F.'
+            name='Jhennie F.'
+            job='Businees Owner'
+            />
+          </div>
+        </div>
 
         </div>
     </div>
--- src/ClientTestimonials.jsx
+++ src/ClientTestimonials.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function ClientTestimonials(props) {
     return (
         <div className="ClientTestimonialsBlock">
-            <h1>Client testimonials</h1>
-            <img src={props.img} alt="img" />
-            <p>{props.description}</p>
-            <h2>{props.name}</h2>
-            <p>{props.job}</p>
+                <img src={props.img} alt="img" />
+                <p>{props.description}</p>
+                <div>
+                    <h2>{props.name}</h2>
+                    <p>{props.job}</p>
+                </div>
+            
         </div>
     )
 }
